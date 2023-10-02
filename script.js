be5572fd30be3e2c4e6173cfa6b126109c0311d5
Bilel Hamitouche <billelmadridista470@gmail.com>
2023-10-02 19:56:07 +0100

    Adding UI to the game

--- script.js
+++ script.js
@@ -1,55 +1,59 @@
 
-// initializing prompt
-const prompt = require('prompt-sync')();
-
 // computer choice
 function getComputerChoice() {
-    let options = ["Rock", "Paper", "Scissors"];
+    const options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random(options.length) * (options.length))];
 }
 
+// results
+const score = document.querySelector('.score');
+score.textContent = 'Score: 0 - 0';
+
+const results = document.querySelector('.results');
+results.textContent = '';
+
+
+let playerScore = 0;
+let computerScore = 0;
 // 1 round of the game
 function playRound(playerSelection, computerSelection) {
-    let player = playerSelection.toLowerCase();
-    let computer = computerSelection.toLowerCase();
+    let result = '';
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
     if (player == computer) {
-	return `You tied ${player} == ${computer}`;
+	result = `You tied! ${playerSelection} == ${computerSelection}`;
     } else if (player == "paper" && computer == "rock" || player == "rock" && computer == "scissors" || player == "scissors" && computer == "paper") {
-	return `You win ${player} beats ${computer}`;
+	playerScore++;
+	result = `You Won! ${playerSelection} beats ${computerSelection}`;
     } else {
-	return `You lost ${computer} beats ${player}`;
+	computerScore++;
+	result = `You Lost! ${computerSelection} beats ${playerSelection}`;
+    }
+    results.textContent = result;
+    score.textContent = `Score: ${playerScore} - ${computerScore}`
+
+    if (playerScore === 5) {
+	results.textContent = 'Winner: Player';
+	score.textContent = '';
+    } else if (computerScore === 5) {
+	results.textContent = 'Winner: Computer';
+	score.textContent = '';
     }
 }
 
+// Buttons
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
 
-// 5 rounds of the game 
-function game() {
-    let score = 0;
-    let computerScore = 0;
-    let choice;
-    let round;
-    for (i = 0; i < 5; i++) {
-	choice = prompt("Choose your weapon: ");
-	round = playRound(choice, getComputerChoice());
-	if (round.includes('You win')) {
-	    score++;
-	} else if (round.includes('You lost')){
-	    computerScore++;
-	} else {
-	}
-	console.log(round);
-	console.log(`Score is: ${score}: ${computerScore}`);
-    }
+rock.addEventListener('click', () => {
+    playRound('Rock', getComputerChoice());
+});
 
-    if (score > computerScore) {
-	console.log("Winner");
-    } else if (score < computerScore) {
-	console.log("Loser");
-    } else {
-	console.log("Tied");
-    }
+paper.addEventListener('click', () => {
+    playRound('Paper', getComputerChoice());
+});
 
-}
-
-// 5 rounds of rock paper scissors 
-game();
+scissors.addEventListener('click', () => {
+    playRound('Scissors', getComputerChoice());
+});
